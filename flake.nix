7008a8750e85899db48f46a3d497d14e33292229
Son Sumin <clvswft03@gmail.com>
2024-06-06 02:31:29 +0900

    refactor: apply flake-parts for flake structuring

--- flake.nix
+++ flake.nix
@@ -1,24 +1,44 @@
 {
-  description = "Basic example of Nix-on-Droid system config.";
+  description = "testfailed's nixos-config for Android (nix-on-droid)";
 
   inputs = {
-    # nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+    # TODO: update nixpkgs and nix-on-droid to v24.05
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
-    # nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nix-on-droid.url = "github:t184256/nix-on-droid/release-23.11";
+    nix-on-droid.inputs.nixpkgs.follows = "nixpkgs";
 
-    nix-on-droid = {
-      # url = "github:t184256/nix-on-droid/release-23.05";
-      url = "github:t184256/nix-on-droid/release-23.11";
-      # url = "github:t184256/nix-on-droid/release-24.05";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
-  outputs = { self, nixpkgs, nix-on-droid }: {
+  outputs = inputs@{ flake-parts, ...  }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      # imports = [ ... ];
 
-    nixOnDroidConfigurations.default = nix-on-droid.lib.nixOnDroidConfiguration {
-      modules = [ ./nix-on-droid.nix ];
-    };
+      systems = [ "aarch64-linux" ];
 
-  };
+      # perSystem = { config, self', inputs', pkgs, system, ...  }: {
+      #   ...
+      # };
+
+      flake = {
+				# nixOnDroidConfigurations.default = nix-on-droid.lib.nixOnDroidConfiguration {
+				# 	modules = [ ./nix-on-droid.nix ];
+				# };
+
+				# TODO: Config for NixOS Server
+				# nixosConfigurations...
+
+				# TODO: Config for NixOS VM
+				# nixosConfigurations...
+
+				# TODO: Config for Macbook Air M1 (using nix-darwin)
+				# darwinConfigurations...
+
+				# Config for Android (using nix-on-droid)
+				nixOnDroidConfigurations.default =
+					inputs.nix-on-droid.lib.nixOnDroidConfiguration {
+						modules = [ ./nix-on-droid.nix ];
+					};
+			};
+		};
 }
